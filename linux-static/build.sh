36c9c934b31c5f204517b12ebabd0301f9171c7b
John Brooks <john.brooks@dereferenced.net>
2017-08-16 19:27:30 -0600

    WIP Out-of-dir build for linux-static also

--- linux-static/build.sh
+++ linux-static/build.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 set -e
 
 ROOT_SRC=`pwd`/src
 ROOT_LIB=`pwd`/lib
+BUILD_DIR=`pwd`/build
 BUILD_OUTPUT=`pwd`/output
 
 cd $ROOT_SRC
@@ -13,13 +16,13 @@ git clean -dfx .
 
 RICOCHET_VERSION=`git describe --tags HEAD`
 
-test -e build && rm -r build
-mkdir build
-cd build
+test -e ${BUILD_DIR} && rm -r ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
 
 export PKG_CONFIG_PATH=${ROOT_LIB}/protobuf/lib/pkgconfig:${PKG_CONFIG_PATH}
 export PATH=${ROOT_LIB}/qt5/bin/:${ROOT_LIB}/protobuf/bin/:${PATH}
-qmake CONFIG+=release OPENSSLDIR="${ROOT_LIB}/openssl/" ..
+qmake CONFIG+=release OPENSSLDIR="${ROOT_LIB}/openssl/" ${ROOT_SRC}/ricochet
 make ${MAKEOPTS}
 cp ricochet ${BUILD_OUTPUT}/ricochet-unstripped
 strip ricochet
@@ -28,11 +31,10 @@ mkdir -p staging/ricochet
 cp ricochet staging/ricochet
 cp ${BUILD_OUTPUT}/tor staging/ricochet
 # XXX
-cp -r ../packaging/linux-static/content/* staging/ricochet/
+cp -r ${ROOT_SRC}/ricochet/packaging/linux-static/content/* staging/ricochet/
 
 cd staging
 tar cfj ${BUILD_OUTPUT}/ricochet-${RICOCHET_VERSION}-static.tar.bz2 ricochet
-cd ../../..
 
 echo "---------------------"
 ls -la ${BUILD_OUTPUT}/
